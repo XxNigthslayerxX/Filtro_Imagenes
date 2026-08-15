70a2ab1f1a13774ff674e816ebcc955232f51b73
XxNigthslayerxX <rusbeluceta24@gmail.com>
2026-08-15 10:19:01 -0400

    Modelos de rutas (Testeados y aprobados)
    Co-authored-by-<RusbelUceta>

---
+++ tests/ApplicationPaths.cs
@@ -0,0 +1,55 @@
+﻿namespace ProcesadorImagenesParalelo.Models;
+
+/// <summary>
+/// Centraliza las rutas utilizadas por la aplicación.
+/// </summary>
+public class ApplicationPaths
+{
+    public string InputDirectory { get; }
+    public string SequentialOutputDirectory { get; }
+    public string ParallelOutputDirectory { get; }
+    public string RecursiveOutputDirectory { get; }
+    public string ParallelBatchOutputDirectory { get; }
+
+    public ApplicationPaths(string baseDirectory)
+    {
+        InputDirectory = Path.Combine(
+            baseDirectory,
+            "Input"
+        );
+
+        SequentialOutputDirectory = Path.Combine(
+            baseDirectory,
+            "Output",
+            "Sequential"
+        );
+
+        ParallelOutputDirectory = Path.Combine(
+            baseDirectory,
+            "Output",
+            "Parallel"
+        );
+        RecursiveOutputDirectory = Path.Combine(
+            baseDirectory,
+            "Output",
+            "Recursive"
+        );
+        ParallelBatchOutputDirectory = Path.Combine(
+            baseDirectory,
+            "Output",
+            "ParallelBatch"
+);
+    }
+
+    /// <summary>
+    /// Crea las carpetas necesarias si todavía no existen.
+    /// </summary>
+    public void CreateDirectories()
+    {
+        Directory.CreateDirectory(InputDirectory);
+        Directory.CreateDirectory(SequentialOutputDirectory);
+        Directory.CreateDirectory(ParallelOutputDirectory);
+        Directory.CreateDirectory(RecursiveOutputDirectory);
+        Directory.CreateDirectory(ParallelBatchOutputDirectory);
+    }
+}
---
+++ tests/ProccesingMode.cs
@@ -0,0 +1,13 @@
+﻿namespace ProcesadorImagenesParalelo.Models;
+
+/// <summary>
+/// Representa la estrategia utilizada para procesar las imágenes.
+/// </summary>
+public enum ProcessingMode
+{
+    Sequential = 1,
+    Parallel = 2,
+    Recursive = 3,
+    ParallelBatch = 4,
+    Comparison = 5
+}
